829da869a35e77d1a05b6528a2b48d7c28f37cb3
Kaushiki Guha <its.kaushikiguha@gmail.com>
2025-09-17 13:41:07 +0000

    Let the date of birth button choose year and  month

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -140,6 +140,9 @@ export default function ProfilePage() {
                                 <PopoverContent className="w-auto p-0" align="start">
                                 <Calendar
                                     mode="single"
+                                    captionLayout="dropdown-buttons"
+                                    fromYear={1900}
+                                    toYear={new Date().getFullYear()}
                                     selected={field.value}
                                     onSelect={field.onChange}
                                     disabled={(date) =>
